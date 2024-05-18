884bd0c8cb363f3ac2485fe0e72f2cad06f9d5a1
Bakoulis George <g.bakoulis@outlook.com>
2024-05-18 15:01:43 +0300

    Delete websitesConfig.js

--- websitesConfig.js
+++
@@ -1,6 +0,0 @@
-// websitesConfig.js
-module.exports = {
-    worldtravellen: { url: 'https://worldtravellen.com/', selector: 'section #polaroid div', filePath: 'posts_worldtravellen.json' },
-    // Add more websites here
-    // example: { example: { url: 'https://example.com/', selector: '.example-selector', filePath: 'posts_example.json' } }
-};
